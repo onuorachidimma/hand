098db456d21640c6ba5086b56e68943d87af3a26
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-06-27 00:54:33 +0100

    Setup BrowserRouter with Homepage as default route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Routes, Route } from 'react-router';
+import Homepage from './components/homepage';
 function App() {
   return (
     <div>
-      
+      <Routes>
+      <Route path='/' element={<Homepage />}/>
+      </Routes>
     </div>
   );
 }
