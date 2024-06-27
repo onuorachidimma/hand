b3f93f79bbf4e4d9e886dd7d95ac58ffc58947e0
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-06-27 04:16:02 +0100

    feat: add & style images in HomePage component

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -3,17 +3,17 @@ import Hands from "../asset/images/Hands.png";
 const Hero = () => {
   return (
     <div className="relative bg-yellow-300">
-      <p className="font-bold text-lg sm:text-2xl md:text-5xl lg:text-7xl leading-tight text-center text-gray-900 absolute top-10 left-10 right-10">
+      <p className="font-bold text-lg sm:text-2xl md:text-5xl lg:text-7xl leading-tight text-center text-gray-900 absolute top-20 w-full lg:w-6/12 left-0 right-0 mx-auto">
         Let’s create something great together.
       </p>
 
       <img
-        className="h-60 md:h-80 lg:h-96 w-full object-cover px-4 md:px-16"
+        className="w-full object-cover px-4 md:px-16"
         src={YelloShape}
         alt=""
       />
       <img
-        className="h-40 md:h-60 lg:h-80 w-full object-cover px-1.25 absolute bottom-0 left-0 right-0 mx-auto"
+        className=" w-full object-cover px-1.25 absolute bottom-0 left-0 right-0 mx-auto"
         src={Hands}
         alt=""
       />
--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -7,9 +7,14 @@ const Homepage = () => {
     return (
       <div>
         <NavBar />
-        <SubHeading subHeading="Who We Are" sectionText="We love what we do and create partnerships with our clients to ensure their digital transformation is positioned for long-term success.We believe that the human dimensions essential to start any successful project and that this is where splendid emotional relationships between the company and people are born."/>
-        
         <Hero />
+        <SubHeading subHeading="Who We Are" sectionText="We love what we do and create partnerships with our clients to ensure their digital transformation is positioned for long-term success.We believe that the human dimensions essential to start any successful project and that this is where splendid emotional relationships between the company and people are born."/>
+
+        <div className="">
+        <img className="w-full object-cover" src={gingerAndOrangeImage} alt="" />
+        </div>
+        
+        
       </div>
     );
   };
